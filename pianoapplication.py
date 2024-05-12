a707b71f06d994c0ec225b0f6dd4543f289a0a2e
adanda23 <abhiramdanda@gmail.com>
2024-05-12 13:08:59 -0500

    Added functionality to black keys. All keys now play the correct note. Want to add labels to the keys.

--- pianoapplication.py
+++ pianoapplication.py
@@ -5,15 +5,16 @@ import time
 import fluidsynth
 import threading
 
-app = QApplication([])
-fs = fluidsynth.Synth()
+app = QApplication([]) # Initializing Qt 
+fs = fluidsynth.Synth() # Initalizing fluidsynth
+fs.setting('synth.gain', 5.0 ) # Setting gain to be higher
 fs.start(driver = 'dsound')  # use DirectSound driver
-sfid = fs.sfload("FluidR3_GM.sf2")  # replace path as needed
+sfid = fs.sfload("regular_piano.sf2")  
 fs.program_select(0, sfid, 0, 0)
 
-midiNoteList = [48, 50, 52, 53, 55, 57, 59, 60, 62, 64, 65, 67, 69, 71, 72, 74, 76, 77, 79, 81, 83]
+midiNaturalList = [48, 50, 52, 53, 55, 57, 59, 60, 62, 64, 65, 67, 69, 71, 72, 74, 76, 77, 79, 81, 83]
+midiAccidentalList = [49, 51, 54, 56, 58, 61, 63, 66, 68, 70, 73, 75, 78, 80, 82]
 
-#8
 class Window(QMainWindow): 
     def __init__(self): 
         super().__init__() 
@@ -23,41 +24,45 @@ class Window(QMainWindow):
         self.show() 
 
     def keyboard(self): 
+            ##
             naturalKeys = []
             for i in range(21):
                 naturalKeys.append(QPushButton("", self)) 
                 naturalKeys[-1].move(100 + i*50,150)
                 naturalKeys[-1].resize(52, 350) 
-                naturalKeys[-1].clicked.connect(self.buttonCallback(i))
+                naturalKeys[-1].clicked.connect(self.buttonCallbackNatural(i))
+            ##
             accidentalKeys = []
             for i in range(3):
-                accidentalKeys.append(QPushButton("", self)) 
-                accidentalKeys.append(QPushButton("", self)) 
-                accidentalKeys[-1].move(133 + i*350,150)
-                accidentalKeys[-2].move(133 + 50 + i*350,150)
-                accidentalKeys[-1].resize(36, 175) 
-                accidentalKeys[-2].resize(36, 175) 
+                for j in range(2):
+                    accidentalKeys.append(QPushButton("", self))
+                    accidentalKeys[-1].move(133 + j*50 + i*350, 150) 
+                    accidentalKeys[-1].resize(36, 175) 
+                    accidentalKeys[-1].clicked.connect(self.buttonCallbackAccidental(i*5+j))
+
+
             for i in range(3):
-                accidentalKeys.append(QPushButton("", self)) 
-                accidentalKeys.append(QPushButton("", self)) 
-                accidentalKeys.append(QPushButton("", self)) 
-                accidentalKeys[-1].move(283 + i*350,150)
-                accidentalKeys[-2].move(283 + 50 + i*350,150)
-                accidentalKeys[-3].move(283 + 100 + i*350,150)
-                accidentalKeys[-1].resize(36, 175) 
-                accidentalKeys[-2].resize(36, 175) 
-                accidentalKeys[-3].resize(36, 175) 
+                for j in range(3):
+                    accidentalKeys.append(QPushButton("", self))
+                    accidentalKeys[-1].move(283 + j*50 + i*350, 150) 
+                    accidentalKeys[-1].resize(36, 175) 
+                    accidentalKeys[-1].clicked.connect(self.buttonCallbackAccidental(i*5+j+2))
+
             for key in accidentalKeys:
                 key.setStyleSheet('''QPushButton {
                                         background-color: rgb(55, 55, 55) ;
                                         border-radius: 5px;
-                                    }''')
+                                        text-align: bottom; 
+                                    }''')            
 
 
 
-    def buttonCallback(self, i):
-        return lambda: self.startNoteThread(midiNoteList[i])
+    def buttonCallbackNatural(self, i):
+        return lambda: self.startNoteThread(midiNaturalList[i])
 
+    def buttonCallbackAccidental(self, i):
+        return lambda: self.startNoteThread(midiAccidentalList[i])
+    
     def startNoteThread(self, key):
         noteThread = threading.Thread(target=self.playsound, args=(key,))
         noteThread.start()
